a9d57c580445b2925ba3d60396b218b71410b310
Lev Azaretov <azaretov123@gmail.com>
2025-07-25 06:00:47 +0300

    edit setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+)
 from rest_framework.response import Response
 
 from api.filters import IngredientFilter, RecipeFilter
